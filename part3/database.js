dbfa720b3808c1a15a3f6077112cb26971c9de4e
Mateo Cuervo <mateo.cuervo@1und1.de>
2019-08-06 15:36:43 +0200

    3.22 completed

--- part3/database.js
+++ part3/database.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connectDatabase = () => {
-  const url = process.env.MONGODB_URI;
-  mongoose.connect(url, {useNewUrlParser: true});
+  const url = process.env.MONGODB_URI
+  mongoose.connect(url, { useNewUrlParser: true })
 };
 
-module.exports = {connectDatabase};
+module.exports = { connectDatabase }
