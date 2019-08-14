8c4f42fe83c87c22e28b451bc1e9dfc50b8898d8
Mateo Cuervo <mateo.cuervo@1und1.de>
2019-08-14 13:50:32 +0200

    part 4.13 completed

--- part4/controllers/blog-controller.js
+++ part4/controllers/blog-controller.js
@@ -1,9 +1,12 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog-model')
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
+  Blog.find({})
     .then(blogs => {
       response.json(blogs)
     })
@@ -11,10 +14,18 @@ blogRouter.get('/', (request, response) => {
 
 blogRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
-  blog
-    .save()
+  blog.save()
     .then(result => {
       response.status(201).json(result)
+    })
+})
+
+blogRouter.delete('/:id', (req, res)=>{
+  const id = req.params.id.toString();
+  Blog.findByIdAndRemove(id)
+    .then(dbRes => {
+      console.log('del res ooooo', dbRes)
+      res.json(dbRes).status(204).end();
     })
 })
 
