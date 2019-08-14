d98b217db3f01606e3de9b0c1185a1e1b68aa6ba
Mateo Cuervo <mateo.cuervo@1und1.de>
2019-08-14 13:30:42 +0200

    part 4.10 completed

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -1,22 +1,45 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
 
-const api = supertest(app)
+const api = supertest(app);
+describe('blog api tests', () => {
 
-test('get blogs as json', async ()=>{
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('GET blogs as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('blogs have field id', async ()=>{
-  const res = await api.get('/api/blogs')
-  expect(res.body).toBeDefined()
-  expect(res.body[0].id).toBeDefined()
-})
+  test('POST a blog as json', async () => {
+    const getBlogs = await api.get('/api/blogs')
+    const initBlogsCount = getBlogs.body.length
 
-afterAll(()=>{
-  mongoose.connection.close()
-})
+    const blog = {
+      'title': 'bar',
+      'author': 'Hyde',
+      'url': 'foo.com',
+      'likes': 100,
+      '__v': 0
+    };
+    await api.post('/api/blogs')
+      .send(blog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const newGetBlogs = await api.get('/api/blogs')
+    const newBlogsCount = newGetBlogs.body.length
+    expect(initBlogsCount + 1).toBe(newBlogsCount)
+  });
+
+  test('blogs have field id', async () => {
+    const res = await api.get('/api/blogs');
+    expect(res.body).toBeDefined();
+    expect(res.body[0].id).toBeDefined();
+  });
+
+});
+afterAll(() => {
+  mongoose.connection.close();
+});
